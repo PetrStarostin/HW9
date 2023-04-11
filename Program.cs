e369038510b30dc872790f37c54e15113e75e453
Petr Starostin <starostin.petr@yandex.ru>
2023-04-11 16:22:18 +0400

    Two Tasks

--- Program.cs
+++ Program.cs
@@ -1,2 +1,37 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии
+
+// void WriteNum(int  N)
+// {
+//     if(N > 1)
+//     {
+//         Console.Write(N + " ");
+//         WriteNum(N - 1);
+//     }
+//     if(N == 1)
+//         Console.Write(N + " ");
+// }
+
+// int N = Convert.ToInt32(Console.ReadLine());
+// WriteNum(N);
+
+//Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N
+
+// void WriteNum(int  M , int N)
+// {
+//     if(M < N)
+//     {
+//         Console.Write(M + " ");
+//         WriteNum(M + 1, N);
+//     }
+//     if(M > N)
+//     {
+//         Console.Write(M + " ");
+//         WriteNum(M - 1, N);
+//     }
+//     if(M == N)
+//         Console.Write(M + " ");
+// }
+
+// int M = Convert.ToInt32(Console.ReadLine());
+// int N = Convert.ToInt32(Console.ReadLine());
+// WriteNum(M,N);
